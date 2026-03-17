95c4f2c76ada55c1c08488f65ed85ff45d452835
Shamil-Youzaf <shamilty02@gmail.com>
2026-03-18 00:07:58 +0530

    Cloudinary Debugging

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,40 +1,49 @@
-import { StrictMode, useState, useEffect } from "react";
+//import { StrictMode, useState, useEffect } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import axios from "./lib/axios.js";
+//import axios from "./lib/axios.js";
 import { BrowserRouter } from "react-router-dom";
 
 // eslint-disable-next-line react-refresh/only-export-components
-function Main() {
-  const [csrfReady, setCsrfReady] = useState(false);
+//function Main() {
+//const [csrfReady, setCsrfReady] = useState(false);
 
-  useEffect(() => {
-    const fetchCsrfToken = async () => {
-      try {
-        await axios.get("/csrf-token");
-        setCsrfReady(true);
-      } catch (error) {
-        console.error("Failed to fetch CSRF token:", error);
-        setCsrfReady(true);
-      }
-    };
-    fetchCsrfToken();
-  }, []);
+//useEffect(() => {
+//const fetchCsrfToken = async () => {
+//  try {
+//    await axios.get("/csrf-token");
+//    setCsrfReady(true);
+//  } catch (error) {
+//    console.error("Failed to fetch CSRF token:", error);
+//   setCsrfReady(true);
+//  }
+// };
+//  fetchCsrfToken();
+// }, []);
 
-  if (!csrfReady) {
-    return null;
-  }
+// if (!csrfReady) {
+// return null;
+//}
 
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
+//return (
+// <BrowserRouter>
+//    <App />
+// </BrowserRouter>
+//);
+//}
+
+///createRoot(document.getElementById("root")).render(
+// <StrictMode>
+// <Main />
+// </StrictMode>,
+//);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Main />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
